9678a2d9c1d43b655ce865fb308d1913cedec23f
Anthony Halim <anthony.halim@live.com>
2021-10-03 23:06:41 +0800

    Add payment intent endpt

--- api/webhook.js
+++ api/webhook.js
@@ -1,5 +1,18 @@
 const stripeAPI = require('../stripe')
 
+const webHookHandlers = {
+  'checkout.session.completed': (data) => {
+    console.log('Checkout completed successfully', data);
+    // Other business logic
+  },
+  'payment_intent.succeeded': (data) => {
+    console.log('Payment succeeded', data);
+  },
+  'payment_intent.payment_failed': (data) => {
+    console.log('Payment failed', data);
+  }
+}
+
 function webhook(req, res) {
   const sig = req.headers['stripe-signature'];
   let event;
@@ -12,9 +25,8 @@ function webhook(req, res) {
     return res.status(400).send(`Webhook error ${error.message}`);
   }
 
-  if (event.type === 'checkout.session.completed') {
-    const session = event.data.object;
-    console.log('Event data', session);
+  if (webHookHandlers[event.type]) {
+    webHookHandlers[event.type](event.data.object)
   }
 }
 
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors  = require('cors');
 require('dotenv').config({path: './.env'});
 const createCheckoutSession = require('./api/checkout');
 const webhook = require('./api/webhook')
+const paymentIntent = require('./api/paymentIntent')
+
 const app = express();
 const port = 8080;
 
@@ -14,6 +16,8 @@ app.use(cors({origin: true}));
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.post('/create-checkout-session', createCheckoutSession);
+
+app.post('/create-payment-intent', paymentIntent);
 
 app.post('/webhook', webhook)
 app.listen(port, () => console.log('server is listening on port', port));
